e7292ebc63a8a56c13403e9a0ea4eaaae2784ddf
giacomochech <76682303+giacomochech@users.noreply.github.com>
2023-06-28 19:38:10 +0200

    deploy fix

--- project/truffle/contracts/authenticator.sol
+++ project/truffle/contracts/authenticator.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract CardRegistry {
+contract Authenticator {
     struct Card {
         string name;
         uint256 grade;
